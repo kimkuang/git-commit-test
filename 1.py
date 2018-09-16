e677cd907bb7cc78ae144a4ed6af903f7f9bbea8
kimkuang <ygtq_kuang@163.com>
2018-09-16 13:40:54 +0800

    chang in master

--- 1.py
+++ 1.py
@@ -1,5 +1,9 @@
 1111
 ####i went to change1
+<<<<<<< HEAD
 ####i was changed branch dev
 #### changed branch dev 2
 #### change for rebase in dev
+=======
+####edit in masdter
+>>>>>>> chang in master
