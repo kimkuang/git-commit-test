616d2405a66e8d08471d59de9ad36155e0e0cac5
kimkuang <ygtq_kuang@163.com>
2018-09-28 15:26:57 +0800

    function 参数初始值

--- function_test.py
+++ function_test.py
@@ -1,1 +1,16 @@
 #test for function
+#def first_function(a,b,c):
+#    a=a+c;
+#    b=a-c;
+#    print(a,b,c);
+
+def sale_car(price,colour='red',brand='camry',is_second_hand=True,length=4000,hight=2000):
+    print('price:',price,
+        'colour:',colour,
+        'brand:',brand,
+        'is_second_hand:',is_second_hand,
+        'length:',length,
+        'hight:',hight)
+    return length+hight
+print(sale_car(1000))
+
