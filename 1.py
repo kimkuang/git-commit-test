56e120cc538916d21c8499a21d0027724dbbe029
kimkuang <ygtq_kuang@163.com>
2018-09-28 10:27:36 +0800

    test if

--- 1.py
+++ 1.py
@@ -1,5 +1,40 @@
-1111
+condition = 1
+while condition<10:
+    print(condition)
+    condition = condition+1
+    print("over?")
+
+print("over")
+
+example_list=[1,2,3,44,534,23,553,44]
+for i in example_list:
+    print(i)
+
+for i in range(1,10):
+    print(i)
+
+tup = ('python',2.7,'windows10')
+for j in tup:
+    print(j)
+
+dic = {}
+dic['ian'] = 'python'
+dic['version'] = 2.7
+dic['platform'] = 'winsows10 64'
+for k in dic:
+    print(k,dic[k])
+
+
 ####i went to change1
 ####i was changed branch dev
 #### changed branch dev 2
 #### change for rebase in dev
+#### i feel happy
+
+x,y,z = 3,1,3
+if x>y:
+    print("x is greater than y")
+elif x==y:
+    print("x is equal to y")
+else:
+    print("x is less than y ")
